cc1cc7d636b606cc5941a8f741c1f2cebcfb1ba0
Hiranyakeshara <81574812+Hiranyakeshara@users.noreply.github.com>
2024-01-26 13:31:41 +0530

    Update db_connect.php

--- Roles/Pharmacist/db_connect.php
+++ Roles/Pharmacist/db_connect.php
@@ -1,3 +1,3 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','medi')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli('localhost','root','','pharmacy')or die("Could not connect to mysql".mysqli_error($con));
